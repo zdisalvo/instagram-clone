3b0a627b9b79b02ca90546f303c5f906fc61807d
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-06-07 23:34:11 -0700

    added styling to active profile route

--- src/components/Sidebar/ProfileLink.jsx
+++ src/components/Sidebar/ProfileLink.jsx
@@ -1,9 +1,18 @@
-import { Avatar, Box, Link, Tooltip } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Avatar, Box, Link, Tooltip, useColorModeValue} from "@chakra-ui/react";
+import { Link as RouterLink, useLocation} from "react-router-dom";
 import useAuthStore from "../../store/authStore";
 
 const ProfileLink = () => {
 	const authUser = useAuthStore((state) => state.user);
+
+	const location = useLocation();
+
+  	
+  	const activeColor = "#15f4EE";
+	const activeStyles = {
+		fontWeight: "bold",
+		activeColor,
+	  };
 
 	return (
 		<Tooltip
@@ -25,9 +34,12 @@ const ProfileLink = () => {
 				p={2}
 				w={{ base: 10, md: "full" }}
 				justifyContent={{ base: "center", md: "flex-start" }}
+				color={location.pathname === `/${authUser?.username}` ? activeColor : "inherit"}
+				//_active={activeStyles} // Apply active styles
+        		//_focus={activeStyles} // Apply active styles
 			>
 				<Avatar size={"sm"} src={authUser?.profilePicURL || ""} />
-				<Box display={{ base: "none", md: "block" }}>Profile</Box>
+				<Box display={{ base: "none", md: "block" }} style={location.pathname === `/${authUser?.username}` ? activeStyles : {}}>Profile</Box>
 			</Link>
 		</Tooltip>
 	);
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Box, Button, Flex, Link, Tooltip, useColorModeValue } from "@chakra-ui/react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
 
 import { BiLogOut } from "react-icons/bi";
@@ -8,30 +8,38 @@ import SidebarItems from "./SidebarItems";
 
 const Sidebar = () => {
 	const { handleLogout, isLoggingOut } = useLogout();
+
+	//
+	const location = useLocation();
+
+  	
+  	const activeColor = useColorModeValue("blue.500", "blue.200");
+
 	return (
 	
 	<Box
+		height={{base: "10vw", md: "60px"}}
 		width={{base: "100vw", md: "70vw"}}
-		alignItems={"center"}
-		justifyContent={center}
+		//alignItems={"center"}
+		//justifyContent={"center"}
 		borderTop={"1px solid"}
 		borderColor={"whiteAlpha.300"}
 		py={4}
 		position={"fixed"}
 		bottom={0}
 
-		//left={{ base: 0, md: "50%" }} // Center horizontally on medium and larger screens
-      	//transform={{ base: "none", md: "translateX(-50%)" }} // Center horizontally on medium and larger screens
+		left={{ base: 0, md: "50%" }} // Center horizontally on medium and larger screens
+      	transform={{ base: "none", md: "translateX(-50%)" }} // Center horizontally on medium and larger screens
       
 
-		left={0}
-		px={{ base: 2, md: 4 }}
+		//left={0}
+		px={{ base: 2, md: 2 }}
 		bg={"gray.900"}
 		zIndex={10}
 
     >
-      <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"} w='full'>
-        <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "flex" }} cursor='pointer'>
+      <Flex direction={"row"} justifyContent={"center"} alignItems={"center"} w='full' height="100%" gap={4}>
+        <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "flex" }} cursor='pointer' >
           <InstagramLogo />
         </Link>
         <Link
@@ -48,7 +56,7 @@ const Sidebar = () => {
         >
           <InstagramMobileLogo />
         </Link>
-        <Flex direction={"row"} gap={5} cursor={"pointer"}>
+        <Flex direction={"row"} gap={5} cursor={"pointer"} justifyContent={"center"}>
           <SidebarItems />
         </Flex>
 
@@ -64,12 +72,12 @@ const Sidebar = () => {
           <Flex
             onClick={handleLogout}
             alignItems={"center"}
-            gap={4}
+            gap={2}
             _hover={{ bg: "whiteAlpha.400" }}
             borderRadius={6}
             p={2}
-            w={{ base: 10, md: "full" }}
-            justifyContent={{ base: "center", md: "flex-start" }}
+            //w={{ base: 10, md: "full" }}
+            justifyContent={{ base: "center", md: "center" }}
           >
             <BiLogOut size={25} />
             <Button
@@ -77,6 +85,12 @@ const Sidebar = () => {
               variant={"ghost"}
               _hover={{ bg: "transparent" }}
               isLoading={isLoggingOut}
+			  //
+			  //px={0}
+              //ml={0}
+              alignItems="center"
+			  fontWeight="normal"
+			  color={location.pathname === '/logout' ? activeColor : "inherit"}
             >
               Logout
             </Button>
