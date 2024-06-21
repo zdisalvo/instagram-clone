262c81cfa23dc1f0842ce69469cee658f46ace50
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-06-21 02:36:53 -0700

    homepage styling changed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
 const HomePage = () => {
 	return (
-		<Container p={0}>
+		<Container p={0} maxW="100vw" m={0}>
 			<Box
 			px={0}
 			mx={0}
@@ -13,15 +13,19 @@ const HomePage = () => {
 			
 			bottom={{base: "10vh", md: "60px"}}
 
-			left={{ base: 0, md: "30%" }} // Center horizontally on medium and larger screens
-			transform={{ base: "none", md: "translateX(-30%)" }} // Center horizontally on medium and larger screens
+			// left={{ base: 0, md: "30%" }} 
+			// transform={{ base: "none", md: "translateX(-30%)" }} 
+
+			left={{ base: 0, md: "0%" }} 
+			transform={{ base: "none", md: "none" }} 
       
 		>
+
 			<Flex gap={0} px={0} mx={0}>
-				<Box flex={1} py={0} px={0}>
+				<Box flex={2} py={0} px={0}>
 					<FeedPostsOrig />
 				</Box>
-				<Box px={0} mx={0} flex={3} mr={{base: "none", md: "20"}} display={{ base: "none", lg: "block" }} maxW={{base: "200px", lg: "300px"}}>
+				<Box px={0} ml={0} flex={3} mr={{base: "none", md: "20"}} display={{ base: "none", lg: "block" }} maxW={{base: "200px", lg: "300px"}}>
 					<SuggestedUsers />
 				</Box>
 			</Flex>
