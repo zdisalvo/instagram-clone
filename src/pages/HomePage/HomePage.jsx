31e4b602978d6120bda2027d3accc14cf5d30845
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-06-20 23:56:44 -0700

    changed HomePage display

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,15 +4,26 @@ import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
 const HomePage = () => {
 	return (
-		<Container maxW={"container.lg"}>
+		<Container>
+			<Box
+			height={{base: "90vh", md: "90vh"}}
+			width={{base: "100vw", md: "70vw"}}
+			
+			bottom={{base: "10vh", md: "60px"}}
+
+			left={{ base: 0, md: "30%" }} // Center horizontally on medium and larger screens
+			transform={{ base: "none", md: "translateX(-30%)" }} // Center horizontally on medium and larger screens
+      
+		>
 			<Flex gap={0}>
-				<Box flex={2} py={10}>
+				<Box flex={2} py={0}>
 					<FeedPostsOrig />
 				</Box>
 				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
 					<SuggestedUsers />
 				</Box>
 			</Flex>
+			</Box>
 		</Container>
 	);
 };
