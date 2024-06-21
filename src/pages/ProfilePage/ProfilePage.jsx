5f65a309a36f2d3a955ddb944c68d6bf788b763a
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-06-21 16:22:02 -0700

    profile page save

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -22,9 +22,7 @@ const ProfilePage = () => {
         width={{ base: "100vw", md: "65vw" }}
         bottom={{ base: "10vh", md: "60px" }}
         left={0}
-        transform="none"
         
-        flexDirection="column"
         justifyContent="center"
         
       >
